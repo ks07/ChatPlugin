39970ae1ff7f4d6e237c205646afd69e6a141928
ks07 <gfield145@gmail.com>
2011-09-13 18:08:13 +0100

    Removed unused imports.

--- src/de/johannes13/minecraft/bukkit/chat/plugin/ChatPlugin.java
+++ src/de/johannes13/minecraft/bukkit/chat/plugin/ChatPlugin.java
@@ -1,6 +1,5 @@
 package de.johannes13.minecraft.bukkit.chat.plugin;
 
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -15,7 +14,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.event.Event.Type;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.config.ConfigurationNode;
